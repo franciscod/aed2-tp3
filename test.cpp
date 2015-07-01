10bf118cb8a1db3ed8abde90b083b13aed5e043a
Iván Pondal <ivan.pondal@gmail.com>
2015-06-30 21:08:40 -0300

    Merge tests and makefile

--- test.cpp
+++ test.cpp
@@ -7,15 +7,17 @@
 #include <string>
 #include <iostream>
 
+// Modulos del grupo
+#include "Pila.h"
+
 using namespace aed2;
 
-/** 
- * Imprime un elemento a un string, en vez de a una pantalla, 
- * a través del operador << 
+/**
+ * Imprime un elemento a un string, en vez de a una pantalla,
+ * a través del operador <<
  */
 template <typename T>
-std::string to_str(const T& t)
-{
+std::string to_str(const T& t){
 	std::stringstream ss;
 	ss << t;
 
@@ -24,14 +26,13 @@ std::string to_str(const T& t)
 
 /**
  * Esta función se puede utilizar para comparar dos colecciones
- * iterables que representen conjuntos, es decir, que no tengan 
+ * iterables que representen conjuntos, es decir, que no tengan
  * elementos repetidos.
  */
 template<typename T, typename S>
-bool Comparar(const T& t, const S& s)
-{
-  typename T::const_Iterador it1 = t.CrearIt();
-  typename S::const_Iterador it2 = s.CrearIt();
+bool Comparar(const T& t, const S& s){
+	typename T::const_Iterador it1 = t.CrearIt();
+	typename S::const_Iterador it2 = s.CrearIt();
 
 	// me fijo si tienen el mismo tamanho
 
@@ -55,17 +56,16 @@ bool Comparar(const T& t, const S& s)
 
 	// me fijo que tengan los mismos elementos
 
-	while( it1.HaySiguiente() )
-	{
+	while( it1.HaySiguiente() ){
 		bool esta = false;
 		it2 = s.CrearIt();
 
 		while( it2.HaySiguiente() ) {
-		  if ( it1.Siguiente() == it2.Siguiente() ) {
-			esta = true;
-			break;
-		  }
-		  it2.Avanzar();
+			if ( it1.Siguiente() == it2.Siguiente() ) {
+				esta = true;
+				break;
+			}
+			it2.Avanzar();
 		}
 
 		if ( !esta ) {
@@ -74,65 +74,124 @@ bool Comparar(const T& t, const S& s)
 			
 		it1.Avanzar();
 	}
-  
-  return true;
+
+	return true;
 }
 
+// Pila
+
+void check_pila_vacia(){
+	Pila<int> pila;
+
+	ASSERT(pila.EsVacia());
+}
+
+void check_pila_apilar(){
+	Pila<int> pila;
+
+	pila.Apilar(3);
+
+	ASSERT_EQ(pila.Tope(), 3);
+
+	pila.Apilar(-3);
+
+	ASSERT_EQ(pila.Tope(), -3);
+}
+
+void check_pila_desapilar(){
+	Pila<int> pila;
+
+	pila.Apilar(3);
+	pila.Apilar(-3);
+
+	pila.Desapilar();
+
+	ASSERT_EQ(pila.Tope(), 3);
+
+	pila.Apilar(42);
+	pila.Apilar(13);
+
+	pila.Desapilar();
+
+	ASSERT_EQ(pila.Tope(), 42);
+}
+
+void check_pila_tamanho(){
+	Pila<int> pila;
+
+	ASSERT_EQ(pila.Tamanho(), 0);
+
+	pila.Apilar(1);
+
+	ASSERT_EQ(pila.Tamanho(), 1);
+
+	pila.Desapilar();
+
+	ASSERT_EQ(pila.Tamanho(), 0);
+}
+
+void check_pila_igualdad(){
+	Pila<int> p1, p2;
+
+	p2.Apilar(4);
+
+	ASSERT(!(p1 == p2));
+
+	p1.Apilar(4);
+
+	ASSERT(p1 == p2);
+}
 
 // ---------------------------------------------------------------------
 
 /**
- * Ejemplo de caso de test, con llamadas a las rutinas de aserción 
- * definidas en mini_test.h
- */
- 
+* Ejemplo de caso de test, con llamadas a las rutinas de aserción
+* definidas en mini_test.h
+* TODO: descomentar cuando esté dcnet
+
 void test_dcnet_ejemplo() {
 	Conj<Interfaz> conjIc1;
 	Conj<Interfaz> conjIc2;
 	Conj<Interfaz> conjIc3;
-	
+
 	conjIc1.Agregar(1);
 	conjIc1.Agregar(2);
 	conjIc1.Agregar(3);
-	
+
 	conjIc2.Agregar(1);
 	conjIc2.Agregar(2);
 	conjIc2.Agregar(3);
-	
+
 	conjIc3.Agregar(1);
 	conjIc3.Agregar(2);
-	
+
 	Computadora c1 = "dc.uba.ar";
 	Computadora c2 = "uba.ar";
 	Computadora c3 = "dm.uba.ar";
-	
+
 	Driver dcnet;
-	
+
 	dcnet.AgregarComputadora(c1, conjIc1);
 	dcnet.AgregarComputadora(c2, conjIc2);
 	dcnet.AgregarComputadora(c3, conjIc3);
-	
+
 	// ejemplo - Indexado en 0 
 	Interfaz i1 = dcnet.IesimaInterfazDe(c1, 0); 
 	Interfaz i2 = dcnet.IesimaInterfazDe(c2, 2);
-	
+
 	dcnet.Conectar(c1, i1, c2, i2);
 	dcnet.CrearPaquete(c1, c2, 3);
 	dcnet.AvanzarSegundo();	
 		
 }
-
-
-int main(int argc, char **argv)
-{
-    RUN_TEST(test_dcnet_ejemplo);
-	
-	/******************************************************************
-	 * TODO: escribir casos de test exhaustivos para todas            *
-	 * las funcionalidades del módulo.                                *
-     * La interacción con el TAD DcNet se debe hacer exclusivamente  *
-	 * a través de la interfaz del driver.                            *
-	 ******************************************************************/
+*/
+int main(int argc, char **argv){
+	// Pila
+	RUN_TEST(check_pila_vacia);
+	RUN_TEST(check_pila_apilar);
+	RUN_TEST(check_pila_desapilar);
+	RUN_TEST(check_pila_tamanho);
+	RUN_TEST(check_pila_igualdad);
 
 	return 0;
 }

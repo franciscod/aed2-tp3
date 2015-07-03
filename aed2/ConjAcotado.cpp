6f07a93df470d2baf9a41b4206125dc497e3156e
Francisco Demartino <demartino.francisco@gmail.com>
2015-07-03 01:16:24 -0300

    fix encoding. iconv al rescate, de latin1 a utf8!

--- aed2/ConjAcotado.cpp
+++ aed2/ConjAcotado.cpp
@@ -4,12 +4,12 @@
 /***************************** ConjAcotado *******************************/
 /*****************************************************************************/
 
-namespace aed2{
+namespace aed2{ 
 
     /**
      * Constructor de un conjunto acotado.
-     * Genera un conjunto vac�o en el rango dado.
-     * Implementa la funci�n VAC�O(in l: nat, in r: nat) -> res : conjAcotado
+     * Genera un conjunto vacío en el rango dado.
+     * Implementa la función VACÍO(in l: nat, in r: nat) -> res : conjAcotado
      * PRE: { l <= r }
      * POS: { res = 0(l,r) }
      * Complejidad: O(r-l)
@@ -22,12 +22,12 @@ namespace aed2{
     /**
      * Constructor por copia de un conjunto acotado.
      * Genera una copia nueva del conjuto.
-     * Implementa la funci�n COPIAR(in c: conjAcotado) -> res : conjAcotado
+     * Implementa la función COPIAR(in c: conjAcotado) -> res : conjAcotado
      * PRE: { true }
      * POS: { res = c }
      * Complejidad: O(Supremo(c) - Infimo(c))
      */
-    ConjAcotado::ConjAcotado(const ConjAcotado& conjunto) :
+    ConjAcotado::ConjAcotado(const ConjAcotado& conjunto) : 
         infimo(conjunto.infimo), pertenencia(conjunto.pertenencia.Tamanho()) {
 
         for(ConjAcotado::const_Iterador it = conjunto.CrearIt(); it.HaySiguiente(); it.Avanzar())
@@ -36,7 +36,7 @@ namespace aed2{
 
     /**
      * Agrega un elemento al conjunto.
-     * Implementa la funci�n AGREGAR(in/out c: conjAcotado, in e : nat)
+     * Implementa la función AGREGAR(in/out c: conjAcotado, in e : nat)
      * PRE: { c = c_0 ^ Infimo(c) <= e <= Supremo(c) }
      * POS: { c = Ag(e, c_0) }
      * Complejidad: O(1)
@@ -49,8 +49,8 @@ namespace aed2{
     }
 
     /**
-     * Devuelve el valor m�nimo que se puede agregar al conjunto.
-     * Implementa la funci�n INFIMO(in c: conjAcotado) -> res : nat
+     * Devuelve el valor mínimo que se puede agregar al conjunto.
+     * Implementa la función INFIMO(in c: conjAcotado) -> res : nat
      * PRE: { true }
      * POS: { res = Infimo(c) }
      * Complejidad: O(1)
@@ -60,8 +60,8 @@ namespace aed2{
     }
 
     /**
-     * Devuelve el valor m�ximo que se puede agregar al conjunto.
-     * Implementa la funci�n SUPREMO(in c: conjAcotado) -> res : nat
+     * Devuelve el valor máximo que se puede agregar al conjunto.
+     * Implementa la función SUPREMO(in c: conjAcotado) -> res : nat
      * PRE: { true }
      * POS: { res = Supremo(c) }
      * Complejidad: O(1)
@@ -71,8 +71,8 @@ namespace aed2{
     }
 
     /**
-     * Devuelve true si y s�lo si c esta vac�o.
-     * Implementa la funci�n ESVAC�O(in c: conjAcotado) -> res : nat
+     * Devuelve true si y sólo si c esta vacío.
+     * Implementa la función ESVACÍO(in c: conjAcotado) -> res : nat
      * PRE: { true }
      * POS: { res = 0?(ConjSuby(c) }
      * Complejidad: O(1)
@@ -82,11 +82,11 @@ namespace aed2{
     }
 
     /**
-     * Devuelve true si y s�lo e pertenece al conjunto.
+     * Devuelve true si y sólo e pertenece al conjunto.
      * Notar que no es requerido que e pertenezca al rango de c.
-     * Implementa la funci�n PERTENECE?(in c: conjAcotado, in e: nat) -> res : bool
+     * Implementa la función PERTENECE?(in c: conjAcotado, in e: nat) -> res : bool
      * PRE: { true }
-     * POS: { res = e � ConjSuby(c) }
+     * POS: { res = e  ConjSuby(c) }
      * Complejidad: O(1)
      */
     bool ConjAcotado::Pertenece(Nat e) const {
@@ -96,10 +96,10 @@ namespace aed2{
     /**
      * Elimina a de c, si es que estaba.
      * Observar que no es requerido que e pertenezca al rango de c.
-     * Implementa la funci�n ELIMINAR(in/out c: conjAcotado, in e: nat)
+     * Implementa la función ELIMINAR(in/out c: conjAcotado, in e: nat)
      * PRE: { c = c_0 }
      * POS: { ConjSuby(c) = ConjSuby(c_0) \ {e} ^ Rango(c) = Rango(c_0) }
-     * Complejidad: O(1)
+     * Complejidad: O(1)		
      */
     void ConjAcotado::Eliminar(Nat e) {
         if (EnRango(e) && Pertenece(e)) {
@@ -107,24 +107,24 @@ namespace aed2{
             pertenencia.Borrar(Indice(e));
         }
     }
-
+    
     /**
      * Crea un iterador modificable.
      */
     ConjAcotado::Iterador ConjAcotado::CrearIt() {
 		return ConjAcotado::Iterador(this);
 	}
-
+	
 	/**
 	 * Crea un iterador constante.
 	 */
 	ConjAcotado::const_Iterador ConjAcotado::CrearIt() const {
-		return ConjAcotado::const_Iterador(this);
+		return ConjAcotado::const_Iterador(this);	
 	}
 
     /**
      * Devuelve la cantidad de elementos del conjunto.
-     * Implementa la funci�n CARDINAL(in c: conjAcotado) -> res : nat
+     * Implementa la función CARDINAL(in c: conjAcotado) -> res : nat
      * PRE: { true }
      * POS: { res = #ConjSuby(c) }
      * Complejidad: O(1)
@@ -133,12 +133,12 @@ namespace aed2{
         return elementos.Cardinal();
     }
 
-    /** M�todo privado para validar que un elemento est� dentro el rango. */
+    /** Método privado para validar que un elemento está dentro el rango. */
     bool ConjAcotado::EnRango(Nat e) const {
         return Infimo() <= e && e <= Supremo();
     }
 
-    /** M�todo privado para obtener el hash de un elemento. PRE: { enRango(e) } */
+    /** Método privado para obtener el hash de un elemento. PRE: { enRango(e) } */
     int ConjAcotado::Indice(Nat e) const {
         return e - infimo;
     }
@@ -150,19 +150,19 @@ namespace aed2{
 
     /**
      * Crea un iterador bidireccional del conjunto, de forma tal que HayAnterior
-     * eval�e a false (i.e., que se pueda recorrer los elementos aplicando
+     * evalúe a false (i.e., que se pueda recorrer los elementos aplicando
      * iterativamente Siguiente).
-     * Implementa la funci�n CrearIt(in c: conjAcotado) -> res : itConj
+     * Implementa la función CrearIt(in c: conjAcotado) -> res : itConj
      * PRE: { true }
-     * POS: { esPermutaci�n?(SecySuby(res), ConjSuby(c)) ^ vacia?(Anteriores(res)) }
+     * POS: { esPermutación?(SecySuby(res), ConjSuby(c)) ^ vacia?(Anteriores(res)) }
      * Complejidad: O(1)
      */
     ConjAcotado::Iterador::Iterador(ConjAcotado* conjunto) :
         it(conjunto->elementos.CrearIt()), conj(conjunto) {}
 
     /**
-     * Devuelve true si en el iterador todav�a quedan elementos por avanzar.
-     * Implementa la funci�n HAYSIGUIENTE(in it: itConj) -> res : bool
+     * Devuelve true si en el iterador todavía quedan elementos por avanzar.
+     * Implementa la función HAYSIGUIENTE(in it: itConj) -> res : bool
      * PRE: { true }
      * POS: { res = haySiguiente?(it) }
      * Complejidad: O(1)
@@ -172,8 +172,8 @@ namespace aed2{
     }
 
     /**
-     * Devuelve true si en el iterador todav�a quedan elementos por retroceder.
-     * Implementa la funci�n HAYANTERIOR(in it: itConj) -> res : bool
+     * Devuelve true si en el iterador todavía quedan elementos por retroceder.
+     * Implementa la función HAYANTERIOR(in it: itConj) -> res : bool
      * PRE: { true }
      * POS: { res = hayAnterior?(it) }
      * Complejidad: O(1)
@@ -183,8 +183,8 @@ namespace aed2{
     }
 
     /**
-     * Devuelve el elemento siguiente a la posici�n del iterador.
-     * Implementa la funci�n SIGUIENTE(in it: itConj) -> res : nat
+     * Devuelve el elemento siguiente a la posición del iterador.
+     * Implementa la función SIGUIENTE(in it: itConj) -> res : nat
      * PRE: { HaySiguiente?(it) }
      * POS: { res = Siguiente(it) }
      * Complejidad: O(1)
@@ -195,8 +195,8 @@ namespace aed2{
     }
 
     /**
-     * Devuelve el elemento anterior a la posici�n del iterador.
-     * Implementa la funci�n ANTERIOR(in it: itConj) -> res : nat
+     * Devuelve el elemento anterior a la posición del iterador.
+     * Implementa la función ANTERIOR(in it: itConj) -> res : nat
      * PRE: { HayAnterior?(it) }
      * POS: { res = Anterior(it) }
      * Complejidad: O(1)
@@ -207,8 +207,8 @@ namespace aed2{
     }
 
     /**
-     * Avanza a la posici�n siguiente del iterador.
-     * Implementa la funci�n AVANZAR(in/out it: itConj)
+     * Avanza a la posición siguiente del iterador.
+     * Implementa la función AVANZAR(in/out it: itConj)
      * PRE: { it = it_0 ^ HaySiguiente?(it) }
      * POS: { it = Avanzar(it_0) }
      * Complejidad: O(1)
@@ -219,8 +219,8 @@ namespace aed2{
     }
 
     /**
-     * Retrocede a la posici�n anterior del iterador.
-     * Implementa la funci�n RETROCEDER(in/out it: itConj)
+     * Retrocede a la posición anterior del iterador.
+     * Implementa la función RETROCEDER(in/out it: itConj)
      * PRE: { it = it_0 ^ HayAnterior?(it) }
      * POS: { it = Retroceder(it_0) }
      * Complejidad: O(1)
@@ -231,9 +231,9 @@ namespace aed2{
     }
 
     /**
-     * Elimina de la lista iterada el valor que se encuentra en la posici�n
+     * Elimina de la lista iterada el valor que se encuentra en la posición
      * siguiente del iterador.
-     * Implmenta la funci�n ELIMINARSIGUIENTE(in/out it: itConj)
+     * Implmenta la función ELIMINARSIGUIENTE(in/out it: itConj)
      * PRE: { it = it_0 ^ HaySiguiente?(it) }
      * POS: { it = EliminarSiguiente(it_0) }
      * Complejidad: O(1)
@@ -245,9 +245,9 @@ namespace aed2{
     }
 
     /**
-     * Elimina de la lista iterada el valor que se encuentra en la posici�n
+     * Elimina de la lista iterada el valor que se encuentra en la posición
      * anterior del iterador.
-     * Implmenta la funci�n ELIMINARANTERIOR(in/out it: itConj)
+     * Implmenta la función ELIMINARANTERIOR(in/out it: itConj)
      * PRE: { it = it_0 ^ HayAnterior?(it) }
      * POS: { it = EliminarAnterior(it_0) }
      * Complejidad: O(1)
@@ -265,19 +265,19 @@ namespace aed2{
 
     /**
      * Crea un iterador bidireccional del conjunto, de forma tal que HayAnterior
-     * eval�e a false (i.e., que se pueda recorrer los elementos aplicando
+     * evalúe a false (i.e., que se pueda recorrer los elementos aplicando
      * iterativamente Siguiente). Este iterador no permite modificar el conjunto.
-     * Implementa la funci�n CrearIt(in c: conjAcotado) -> res : itConj
+     * Implementa la función CrearIt(in c: conjAcotado) -> res : itConj
      * PRE: { true }
-     * POS: { esPermutaci�n?(SecySuby(res), ConjSuby(c)) ^ vacia?(Anteriores(res)) }
+     * POS: { esPermutación?(SecySuby(res), ConjSuby(c)) ^ vacia?(Anteriores(res)) }
      * Complejidad: O(1)
      */
-    ConjAcotado::const_Iterador::const_Iterador(const ConjAcotado* conjunto) :
+    ConjAcotado::const_Iterador::const_Iterador(const ConjAcotado* conjunto) : 
         it(conjunto->elementos.CrearIt()) {}
 
     /**
-     * Devuelve true si en el iterador todav�a quedan elementos por avanzar.
-     * Implementa la funci�n HAYSIGUIENTE(in it: itConj) -> res : bool
+     * Devuelve true si en el iterador todavía quedan elementos por avanzar.
+     * Implementa la función HAYSIGUIENTE(in it: itConj) -> res : bool
      * PRE: { true }
      * POS: { res = haySiguiente?(it) }
      * Complejidad: O(1)
@@ -287,8 +287,8 @@ namespace aed2{
     }
 
     /**
-     * Devuelve true si en el iterador todav�a quedan elementos por retroceder.
-     * Implementa la funci�n HAYANTERIOR(in it: itConj) -> res : bool
+     * Devuelve true si en el iterador todavía quedan elementos por retroceder.
+     * Implementa la función HAYANTERIOR(in it: itConj) -> res : bool
      * PRE: { true }
      * POS: { res = hayAnterior?(it) }
      * Complejidad: O(1)
@@ -298,8 +298,8 @@ namespace aed2{
     }
 
     /**
-     * Devuelve el elemento siguiente a la posici�n del iterador.
-     * Implementa la funci�n SIGUIENTE(in it: itConj) -> res : nat
+     * Devuelve el elemento siguiente a la posición del iterador.
+     * Implementa la función SIGUIENTE(in it: itConj) -> res : nat
      * PRE: { HaySiguiente?(it) }
      * POS: { res = Siguiente(it) }
      * Complejidad: O(1)
@@ -310,8 +310,8 @@ namespace aed2{
     }
 
     /**
-     * Devuelve el elemento anterior a la posici�n del iterador.
-     * Implementa la funci�n ANTERIOR(in it: itConj) -> res : nat
+     * Devuelve el elemento anterior a la posición del iterador.
+     * Implementa la función ANTERIOR(in it: itConj) -> res : nat
      * PRE: { HayAnterior?(it) }
      * POS: { res = Anterior(it) }
      * Complejidad: O(1)
@@ -322,8 +322,8 @@ namespace aed2{
     }
 
     /**
-     * Avanza a la posici�n siguiente del iterador.
-     * Implementa la funci�n AVANZAR(in/out it: itConj)
+     * Avanza a la posición siguiente del iterador.
+     * Implementa la función AVANZAR(in/out it: itConj)
      * PRE: { it = it_0 ^ HaySiguiente?(it) }
      * POS: { it = Avanzar(it_0) }
      * Complejidad: O(1)
@@ -334,8 +334,8 @@ namespace aed2{
     }
 
     /**
-     * Retrocede a la posici�n anterior del iterador.
-     * Implementa la funci�n RETROCEDER(in/out it: itConj)
+     * Retrocede a la posición anterior del iterador.
+     * Implementa la función RETROCEDER(in/out it: itConj)
      * PRE: { it = it_0 ^ HayAnterior?(it) }
      * POS: { it = Retroceder(it_0) }
      * Complejidad: O(1)
@@ -347,7 +347,7 @@ namespace aed2{
 
     /**
      * Compara c1 y c2 por igualdad.
-     * Implementa la funci�n * = * (in c1: conjAcotado, in c2: conjAcotado) -> bool
+     * Implementa la función * = * (in c1: conjAcotado, in c2: conjAcotado) -> bool
      * PRE: { true }
      * POS: { res = (c1 = c2) }
      * Complejidad: O(min(#c1, #c2))
@@ -363,7 +363,7 @@ namespace aed2{
     }
 
     /**
-     * Genera una representaci�n del conjunto con un string y la escribe dentro
+     * Genera una representación del conjunto con un string y la escribe dentro
      * de un output stream.
      */
     std::ostream& operator<<(std::ostream& os, const ConjAcotado& c) {
